aebcb6bb4da80bde53777a379e214d3816949d19
sanCode <santoni251@hotmail.com>
2026-09-17 12:47:31 -0500

    subir certificado tls a git

--- config/database.php
+++ config/database.php
@@ -60,9 +60,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ], fn ($value) => !is_null($value) && $value !== '') : [],
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA') 
+                    ? storage_path(str_replace('storage/', '', env('MYSQL_ATTR_SSL_CA'))) 
+                    : null,
+            ]) : [],
         ],
 
         'mariadb' => [
